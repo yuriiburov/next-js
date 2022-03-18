aa987c27a689fa8f23637f8c2fa620f2ea130c24
yuriiburov <burovyuriireact@gmail.com>
2022-03-18 19:50:01 +0200

    lesson 5

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.scss';
+import { useRouter } from "next/router";
+
+const navigation = [
+  { id: 1, title: 'Home', path: '/' },
+  { id: 2, title: 'Posts', path: '/posts' },
+  { id: 3, title: 'Contacts', path: '/contacts' }
+]
+
+const Navbar = () => {
+  const { pathname } = useRouter()
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logo}>Lesson 5</div>
+      <div className={styles.links}>
+        {
+          navigation.map(({ id, title, path }) =>
+            <Link key={id} href={path}>
+              <a className={pathname === path ? styles.active : null}>{title}</a>
+            </Link>)
+        }
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Heading from "../components/Heading";
+import styles from '../styles/404.module.scss';
 
-const Error = () => (
-    <>
+const Error = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000)
+  }, [router])
+
+  return (
+    <div className={styles.wrapper}>
       <Heading text='404'/>
       <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, ea!</h2>
-    </>
+    </div>
   );
+}
 
 export default Error;
